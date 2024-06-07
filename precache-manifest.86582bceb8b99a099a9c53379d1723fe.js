f709f4c025d70f66ef1a8986bfda7369747bcbeb
masoud <ma.dashtifard@gmail.com>
2024-06-07 20:44:21 +0330

    Updates

---
+++ precache-manifest.86582bceb8b99a099a9c53379d1723fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9bb75d2b43fff9006ae",
+    "url": "/todo/static/css/main.14b7cb3d.chunk.css"
+  },
+  {
+    "revision": "a9bb75d2b43fff9006ae",
+    "url": "/todo/static/js/main.a9bb75d2.chunk.js"
+  },
+  {
+    "revision": "9dda1a096537b297cf02",
+    "url": "/todo/static/js/1.9dda1a09.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "72d77f22fd1ddf6d9517d3e9c2eaa2d4",
+    "url": "/todo/index.html"
+  }
+];
